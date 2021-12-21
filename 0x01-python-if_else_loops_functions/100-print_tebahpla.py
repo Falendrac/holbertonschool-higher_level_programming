0cbc7b61fb6f760bcdc3e3aead2675f3bd4b5562
Falendrac <leger_franck@hotmail.fr>
2021-12-21 13:33:19 +0000

    Fix task 100, using singly element

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for element in range(25, -1, -1):
     if element % 2 != 0:
-        dif = 32
-    print(chr(element + 65 + dif), end="")
+        element += 32
+    print(chr(element + 65), end="")
