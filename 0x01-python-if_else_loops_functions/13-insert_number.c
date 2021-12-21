0e3f7863b79f116d6282a0e3f7e256a8b51c0d00
Falendrac <leger_franck@hotmail.fr>
2021-12-21 10:31:46 +0000

    Add task 13 to 0x01 directory

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * insert_node - Insert a node by sort
+ *
+ * @head: The adress of the linked list
+ * @number: The number we want to insert
+ *
+ * Return: NULL if head is null or the malloc failled
+ * new otherwise
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new, *old, *current;
+
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	new->next = NULL;
+
+	current = *head;
+	while (current != NULL && current->n < number)
+	{
+		old = current;
+		current = current->next;
+	}
+
+	new->next = current;
+	old->next = new;
+
+	return (new);
+}
