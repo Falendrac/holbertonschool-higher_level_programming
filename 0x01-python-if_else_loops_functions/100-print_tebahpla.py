d9bdc04826f85d2aab08e31a7b1e90b59507da21
Falendrac <leger_franck@hotmail.fr>
2021-12-21 13:38:22 +0000

    Fix 100 use a correct format

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for element in range(25, -1, -1):
-    if element % 2 != 0:
+for element in range(90, 64, -1):
+    if element % 2 == 0:
         element += 32
-    print(chr(element + 65), end="")
+    print("{:c}".format(element), end="")
