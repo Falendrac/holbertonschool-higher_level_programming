45f764da34347e8a5fd8a55934edb78abc47d082
Falendrac <leger_franck@hotmail.fr>
2021-12-21 13:28:54 +0000

    Fix 100 task to not store characters in a variable

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-i = 25
 dif = 0
-for element in range(65, 91):
+for element in range(25, -1, -1):
     if element % 2 != 0:
         dif = 32
-    print(chr(element + i + dif), end="")
-    i -= 2
+    print(chr(element + 65 + dif), end="")
     dif = 0
