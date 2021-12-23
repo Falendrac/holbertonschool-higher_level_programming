4bc07fac59a8e05c5b13bdbc1fbf221dc3f91f48
Falendrac <leger_franck@hotmail.fr>
2021-12-23 11:26:30 +0000

    Test task 100 for checker if import need to be in if

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+import calculator_1
+import sys
 if __name__ == '__main__':
-    import calculator_1
-    import sys
-
     length = len(sys.argv)
     op = ['*', '+', '-', '/']
     argv = sys.argv
