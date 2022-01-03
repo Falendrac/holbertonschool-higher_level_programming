ee530b48bab063079848dc9d3b34a59c42e709db
Falendrac <leger_franck@hotmail.fr>
2022-01-03 11:39:29 +0000

    Fix task 3 using the function reversed for simplify the code

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     length_list = len(my_list)
-    for loop in range(-1, -length_list - 1, -1):
-        print("{:d}".format(my_list[loop]))
+    for loop in reversed(my_list):
+        print("{:d}".format(loop))
