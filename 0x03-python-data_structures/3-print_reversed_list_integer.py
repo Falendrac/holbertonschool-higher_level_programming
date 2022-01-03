19e4df4e671e8a819f0ecadffcd187e0751536bc
Falendrac <leger_franck@hotmail.fr>
2022-01-03 11:35:28 +0000

    Fix task 3 to have the good format

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     length_list = len(my_list)
     for loop in range(-1, -length_list - 1, -1):
-        print("{}".format(my_list[loop]))
+        print("{:d}".format(my_list[loop]))
