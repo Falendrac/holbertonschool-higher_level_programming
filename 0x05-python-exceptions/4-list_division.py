28d6f56a3cef0f1341669e38eafe8838f1ebcd7f
Falendrac <leger_franck@hotmail.fr>
2022-01-17 14:05:26 +0000

    Change task 4 to return when out of range

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -10,6 +10,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("wrong type")
         except IndexError:
             print("out of range")
+            return new_list
         finally:
             pass
     return new_list
