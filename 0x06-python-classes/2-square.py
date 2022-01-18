f7446b7a17d45b8431c1979b72ddc88f35bb8bda
Falendrac <leger_franck@hotmail.fr>
2022-01-18 09:09:28 +0000

    Add task 2 to 0x06 directory

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""1-square.py:
+    The second task for the first project with the POO in python.
+
+    Here we define a square
+"""
+
+
+class Square:
+    """
+    square is aclass that defines a square
+
+    ...
+
+    Attributes
+    ----------
+
+    __size : int
+        define the size of the square, that a private class attribute
+    """
+
+    def __init__(self, size=0):
+        """
+        Test if Size is an integer and if size is inferior at 0
+
+        Parameters
+        ----------
+        __size: int
+            the size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
