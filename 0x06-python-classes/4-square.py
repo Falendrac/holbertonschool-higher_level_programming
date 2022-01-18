5354fc2c19122409a16ad0a41164fe6499dd4cb2
Falendrac <leger_franck@hotmail.fr>
2022-01-18 10:18:34 +0000

    Add task 4 to 0x06 directory

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""4-square.py:
+    The Fifth task for the first project with the POO in python.
+
+    Here we define a square
+"""
+
+
+class Square:
+    """
+    square is a class that defines a square
+
+    ...
+
+    Attributes
+    ----------
+
+    __size : int
+        define the size of the square, that a private class attribute
+
+    size : int
+        the size of the square
+
+    Methods
+    -------
+    size(self)
+        that the getter of the class
+
+    size(self, value)
+        that the setter of the class
+
+    area(self)
+        return the current square area
+    """
+
+    def __init__(self, size=0):
+        """
+        Parameters
+        ----------
+        size: int
+            the size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """The getter of size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        The setter of size
+
+        Parameters
+        ----------
+        value: int
+            the size of the square
+
+        Raises
+        ------
+        Typerror
+            If size is not an integer
+        ValueError:
+            If size is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Return the value of the current square area"""
+        return self.__size * self.__size
