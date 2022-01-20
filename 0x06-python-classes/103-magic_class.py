0c1480ae9fece5acc9bba38dac0fed160b61c496
Falendrac <leger_franck@hotmail.fr>
2022-01-20 13:14:57 +0000

    Fix 103 adding documentation

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,9 +1,38 @@
 #!/usr/bin/python3
+"""
+103-magic_class
+---------------
+
+This task is a retranscription of Bytecode into python
+"""
 import math
 
 
 class MagicClass:
+    """
+    MagicClass - This is the Magic class produce with Bytecode
+
+    ...
+
+    Attributes
+    ----------
+    _MagiClass__radis : int or float
+        The radius of magicclass
+
+    Methods
+    -------
+    area(self)
+        The area of the magic class
+    circumference(self)
+        The circumference of the magic class
+    """
     def __init__(self, radius=0):
+        """
+        Parameters
+        ----------
+        radius : int or float
+            radius define the magicclass
+        """
         self._MagicClass__radius = 0
 
         if type(radius) is not int and type(radius) is not int:
@@ -12,7 +41,13 @@ class MagicClass:
             self._MagicClass__radius = radius
 
     def area(self):
+        """
+        area - define an area
+        """
         return self._MagicClass__radius ** 2 * math.pi
 
     def circumference(self):
+        """
+        circumference - define the circumference
+        """
         return 2 * math.pi * self._MagicClass__radius
