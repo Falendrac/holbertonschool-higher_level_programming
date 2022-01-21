20cdef06c7a25927726860873d0671d2cc3c24b6
Falendrac <leger_franck@hotmail.fr>
2022-01-21 14:05:01 +0000

    Update task 0 to add doc

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,33 @@
 #!/usr/bin/python3
+"""
+This is the first task for the project 0x07 python
+
+...
+
+Methods
+-------
+add_integer(a, b=98)
+    return the addition of two integer and/or float
+
+Example for the function add_integer:
+
+>>> add_integer(2, 3)
+5
+"""
 import doctest
 
 def add_integer(a, b=98):
-    """Returns the addition of a and b
+    """
+    Returns the addition of a and b
+
+    ...
+
+    Parameters
+    ----------
+    a : int/float
+        first parameters to do addition
+    b : int/float
+        first parameters to do addition
     """
     if not isinstance(a, (int, float)):
         raise TypeError('a must be an integer')
