d8ef029480327fb222cfd95e308a05993f2569f9
Falendrac <leger_franck@hotmail.fr>
2022-01-24 08:05:16 +0000

    Add task 0 to 0x08 directory

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+File for the class rectangle.
+
+That define a rectangle.
+"""
+
+
+class Rectangle:
+    """
+    Define a Rectangle
+
+    ...
+
+    Attributes
+    ----------
+
+    Methods
+    -------
+    """
+    pass
