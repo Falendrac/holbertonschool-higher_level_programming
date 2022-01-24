c8fa0336ae026c41b79982297d7c756c5164b99f
Falendrac <leger_franck@hotmail.fr>
2022-01-24 08:21:40 +0000

    Fix task 1, correct name for methods and good order for attribution

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,9 +6,6 @@ That define a rectangle.
 """
 
 
-from turtle import width
-
-
 class Rectangle:
     """
     Define a Rectangle
@@ -39,9 +36,10 @@ class Rectangle:
     height(self, value)
         the setter of height
     """
+
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
@@ -51,16 +49,20 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if width < 0:
+        elif value < 0:
             raise TypeError("width must be >= 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
         return self.__height
 
     @height.setter
-    def haight(self, value):
+    def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise TypeError("height must be >= 0")
+        else:
+            self.__height = value
