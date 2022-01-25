5059281bc1e10d7ad701d837f046b06541b8beef
Falendrac <leger_franck@hotmail.fr>
2022-01-25 07:59:33 +0000

    The checker want the code to print width error and not height error pls checker

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -52,8 +52,8 @@ class Rectangle:
         height : int
             the height of the rectangle
         """
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
