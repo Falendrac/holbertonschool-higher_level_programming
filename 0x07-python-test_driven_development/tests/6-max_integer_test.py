45bd234d30628362a638e97d154ee70a4f8c60d2
Falendrac <leger_franck@hotmail.fr>
2022-01-26 09:38:19 +0000

    Task 5 adding more tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,3 +8,9 @@ class TestIntMax(unittest.TestCase):
 
     def test_simple_integer(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([4, 1, 2, 3]), 4)
+        self.assertEqual(max_integer([1, 2, 4, 3]), 4)
+        self.assertEqual(max_integer([1, 2, -4, 3]), 3)
+        self.assertEqual(max_integer([-1, -2, -4, -3]), -1)
+        self.assertEqual(max_integer([4]), 4)
+        self.assertEqual(max_integer([]), None)
