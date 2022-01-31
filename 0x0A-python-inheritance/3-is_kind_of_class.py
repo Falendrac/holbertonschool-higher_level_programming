a473633ad8c28ce172b08340221a3fcd51bb680b
Falendrac <leger_franck@hotmail.fr>
2022-01-31 09:33:27 +0000

    Add task 3 to 0x0A directory

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''
+Task 3, test if obj is istance of a_class
+'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''Return true if obj is istance of a_class, otherwise return False'''
+    return isinstance(obj, a_class)
