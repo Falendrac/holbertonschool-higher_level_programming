5bff10a72176a2a44723c2da0846e5ce01cd3579
Falendrac <leger_franck@hotmail.fr>
2022-01-31 09:29:19 +0000

    Add task 2 to 0x0A

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''
+Task 2 for test a class object
+'''
+
+
+def is_same_class(obj, a_class):
+    '''return true if obj is the type of a_class, false otherwise'''
+    return type(obj) is a_class
