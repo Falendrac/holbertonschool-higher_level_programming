14e6badb910a3cec9b4d749327c15efb12f9dcbd
Falendrac <leger_franck@hotmail.fr>
2022-01-31 10:08:35 +0000

    Add task 6 to 0x0A directory

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''
+File of the class BaseGeometry
+
+That define the base of the Geometry
+'''
+
+
+class BaseGeometry():
+    '''
+    Define the base of the Geometry
+
+    ...
+
+    Attributes
+    ----------
+
+    Methods
+    -------
+    area(self)
+        calculate the area
+    '''
+    def area(self):
+        '''
+        Calculate the area
+
+        ...
+
+        Raises
+        ------
+        Exception
+            not implemented
+        '''
+        raise Exception("area() is not implemented")
