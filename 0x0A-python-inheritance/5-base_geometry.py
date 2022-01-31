85d2ff09183301e734b494bb73cb17c38501f170
Falendrac <leger_franck@hotmail.fr>
2022-01-31 10:06:04 +0000

    Add task 5 to 0x0A

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+File of the class BaseGeometry
+
+That define the base of the Geometry
+'''
+
+
+class BaseGeometry():
+    '''
+    Define the base of the Geometry
+
+    ...
+
+    Attributes
+    ----------
+
+    Methods
+    -------
+    '''
+    pass
