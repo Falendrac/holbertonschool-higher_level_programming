cdf0c15701cf5bdec71be0a306f1b360b90469d4
Falendrac <leger_franck@hotmail.fr>
2022-02-01 08:41:04 +0000

    Update task 0, adding comments and close function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+'''
+module for open and read a file given in name
+'''
+
+
 def read_file(filename=""):
+    '''
+    open and read a file given in parameter
+
+    Parameter
+    ---------
+    filename : str
+        the name of the file we want to open
+    '''
     with open(filename) as fd:
         for line in fd:
             print("{}".format(line), end="")
+
+    fd.close()
