bd015c911439bc2867b4db75cb7b25d6ce005fc7
Falendrac <leger_franck@hotmail.fr>
2022-02-03 11:37:20 +0000

    Update rectangle to do a better to_dictionary

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -168,4 +168,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """return the the dictionnary representation of Rectangle"""
-        return vars(self)
+        return {'x': self.x, 'y': self.y, 'id': self.id,
+                'height': self.height, 'width': self.width}
