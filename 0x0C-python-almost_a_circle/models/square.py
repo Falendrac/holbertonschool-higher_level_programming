afa1832b3f66ebb8dd8e3aac61b2a6500fddde78
Falendrac <leger_franck@hotmail.fr>
2022-02-03 11:05:59 +0000

    Add the square class to 0x0C

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Models for a Square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Define a Square with inheritance of Rectangle class
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """init method for square"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """return the string representation of square"""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width
+            )
