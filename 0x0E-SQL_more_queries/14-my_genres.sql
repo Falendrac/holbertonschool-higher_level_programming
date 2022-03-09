c4de2ef70770bbeb7c105f48935a83fb5af7659c
Falendrac <leger_franck@hotmail.fr>
2022-03-09 15:39:23 +0100

    Add task 14 to 0x0E.

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- lists all genres of the show Dexter.
+SELECT tv_shows.title, tv_genres.name
+    FROM tv_show_genres
+    JOIN tv_shows on tv_shows.id = tv_show_genres.show_id
+    JOIN tv_genres on tv_genres.id = tv_show_genres.genre_id
+    WHERE tv_shows.title = "Dexter"
+    ORDER BY tv_genres.name;
