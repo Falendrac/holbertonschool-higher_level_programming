2a643816b3284285860a3eacdb9ef3cdeeaa5255
Falendrac <leger_franck@hotmail.fr>
2022-03-09 16:04:15 +0100

    Add task 16 to 0x0E.

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+SELECT tv_shows.title, tv_genres.name
+    FROM tv_show_genres
+    RIGHT JOIN tv_shows on tv_show_genres.show_id = tv_shows.id 
+    JOIN tv_genres on tv_show_genres.genre_id = tv_genres.id
+    ORDER BY tv_shows.title, tv_genres.name;
