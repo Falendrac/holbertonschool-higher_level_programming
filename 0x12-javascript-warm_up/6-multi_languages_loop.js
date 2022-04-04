4ec2adff6f432cec4fc860423064e06959b993cd
Falendrac <leger_franck@hotmail.fr>
2022-04-04 10:32:05 +0200

    Add task 6 to 0x12.

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/* prints 3 lines:
+The first line: “C is fun”
+The second line: “Python is cool”
+The third line: “JavaScript is amazing”
+*/
+
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let loop = 0; loop < array.length; loop++) {
+  console.log(array[loop]);
+}
