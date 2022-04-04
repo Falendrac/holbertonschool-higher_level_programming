4790566150df091535308992f0c79f542c7e88ec
Falendrac <leger_franck@hotmail.fr>
2022-04-04 12:15:14 +0200

    Fix 5, forgot semicolon in line 11.

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -8,5 +8,5 @@ const process = require('process');
 if (isNaN(process.argv[2])) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' + parseInt(process.argv[2]))
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
