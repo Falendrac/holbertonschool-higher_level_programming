73426ddccc54fbfe9bfdc821c7266179017a88f9
Falendrac <leger_franck@hotmail.fr>
2022-04-04 10:26:05 +0200

    Add task 5 to 0x12.

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/* prints My number: <first argument converted in integer>
+if the first argument can be converted to an integer */
+
+import process from 'process';
+
+if (isNaN(process.argv[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]))
+}
