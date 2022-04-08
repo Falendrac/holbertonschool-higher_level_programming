da2c518e1541844f01df590234a39785ba66a393
Falendrac <leger_franck@hotmail.fr>
2022-04-08 10:45:17 +0200

    Fix pycode style to model_state.

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,10 +9,13 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """
     Class definition of state that inherit of Base
     """
     __tablename__ = 'states'
-    id = Column(Integer, Sequence('user_id_seq'), primary_key=True, nullable=False)
+    id = Column(
+        Integer, Sequence('user_id_seq'), primary_key=True, nullable=False
+        )
     name = Column(String(128), nullable=False)
