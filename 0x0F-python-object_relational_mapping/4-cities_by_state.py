e9cc654189ffdfc56f87026614274ea3de01d326
Falendrac <leger_franck@hotmail.fr>
2022-04-08 09:56:06 +0200

    Add task 4 to 0x0F.

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""Script that select states starting with N in a MySQL db"""
+
+import MySQLdb
+import sys
+
+
+def selectStates():
+    """Connect to the DB, select states and print all states"""
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+        )
+    cur = db.cursor()
+    cur.execute(
+        "SELECT cities.id, cities.name, states.name \
+        FROM cities JOIN states ON cities.state_id = states.id \
+        ORDER BY cities.id"
+        )
+
+    for row in cur:
+        print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    selectStates()
