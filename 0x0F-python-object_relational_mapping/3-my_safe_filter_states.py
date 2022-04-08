a2f97859498e6da596d926ae8b65910177791c24
Falendrac <leger_franck@hotmail.fr>
2022-04-08 09:46:51 +0200

    Add task 3 to 0x0F.

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""Script that select states starting with N in a MySQL db"""
+
+import MySQLdb
+import sys
+
+
+def selectStates():
+    """Connect to the DB, select states and print all states"""
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+        )
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name = %(name)s ORDER BY id",
+        {'name': sys.argv[4]})
+
+    for row in cur:
+        print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    selectStates()
