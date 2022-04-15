e48b04759b26e51181908e1bcd49240c2c9ceb1b
Falendrac <leger_franck@hotmail.fr>
2022-04-15 10:55:18 +0200

    Delete an useless line.

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -14,7 +14,6 @@ if __name__ == '__main__':
         'mysql+mysqldb://{}:{}@localhost/{}'.format(
             sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
 
     with Session() as session:
