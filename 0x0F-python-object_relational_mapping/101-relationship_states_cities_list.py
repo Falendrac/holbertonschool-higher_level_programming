f268cbb7e11d318c32eaf71ab1aa8e63c043434c
Falendrac <leger_franck@hotmail.fr>
2022-04-15 10:47:13 +0200

    Add task 101 to 0x0F.

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""print the first element of a database"""
+
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (MetaData, create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    with Session() as session:
+        resultCity = session.query(City, State).filter(
+            State.id == City.state_id
+            ).all()
+
+        stateid = 0
+        for rowCity, rowState in resultCity:
+            if stateid != rowState.id:
+                print("{}: {}".format(rowState.id, rowState.name))
+                stateid = rowState.id
+            print(
+                "   {}: {}".format(rowCity.id, rowCity.name)
+                )
