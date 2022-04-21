64e10daed0f350a1728196369e607deb9f918904
Falendrac <leger_franck@hotmail.fr>
2022-04-21 12:07:24 +0200

    Fix 0 delete the if statement.

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL, and displays the size of the body of the response
-if [ $# -ge 1 ]
-    then
-        curl -sI "${1}" | grep -i Content-Length | cut -d " " -f2
-fi
+curl -sI "${1}" | grep -i Content-Length | cut -d " " -f2
