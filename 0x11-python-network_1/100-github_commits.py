446011ccbd6b5685e7170bcea1aad5c9bfa05918
Falendrac <leger_franck@hotmail.fr>
2022-04-22 15:02:16 +0200

    Add task 100 to 0x11.

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+'''
+
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+
+
+def main():
+    '''
+    Main function
+    '''
+    url = 'https://api.github.com/repos/' + sys.argv[2] + '/' +\
+        sys.argv[1] + '/commits'
+    req = requests.get(url)
+
+    api_info = req.json()
+
+    for i in range(0, 10):
+        print("{}: {}".format(api_info[i].get('sha'),
+                              api_info[i].get('commit')
+                              .get('author').get('name')))
+
+
+if __name__ == '__main__':
+    main()
