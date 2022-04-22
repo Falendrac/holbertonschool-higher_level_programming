0b3d1d352266a27ed9a07a136412eb40b643ba0b
Falendrac <leger_franck@hotmail.fr>
2022-04-22 12:19:09 +0200

    Fix 0 use many print not only one.

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -12,10 +12,10 @@ def main():
     '''
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print("Body response:\n\
-    \t- type: {}\n\
-    \t- content: {}\n\
-    \t- utf8 content: {}".format(type(html), html, html.decode('utf-8')))
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
 
 
 if __name__ == '__main__':
