62d1b870091b7db665de117d205b037e2ad7b022
Falendrac <leger_franck@hotmail.fr>
2022-04-22 11:20:25 +0200

    Add task 5 to 0x11.

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+takes in a URL, sends a request to the URL and displays the value
+of the variable X-Request-Id in the response header
+'''
+
+import requests
+import sys
+
+
+def main():
+    '''
+    Main function
+    '''
+    req = requests.get(sys.argv[1])
+    print(req.headers['X-Request-Id'])
+
+
+if __name__ == '__main__':
+    main()
