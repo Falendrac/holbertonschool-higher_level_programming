21605e6f38e1897c17d157a6479d8652e1668921
Falendrac <leger_franck@hotmail.fr>
2022-04-26 10:58:54 +0200

    Fix 101, integrate @ in ".

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -X "POST" "$1" -H 'Content-Type: application/json' -d @"$2"
+curl -X "POST" "$1" -H 'Content-Type: application/json' -d "@$2"
