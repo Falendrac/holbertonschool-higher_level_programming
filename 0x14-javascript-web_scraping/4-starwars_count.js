77259ed090f759ab13daf39f3da151874ccf86f8
Falendrac <leger_franck@hotmail.fr>
2022-05-17 14:17:35 +0200

    Deplace let count in then statement task 4.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,10 +4,10 @@
  where the character “Wedge Antilles” is present.
 */
 const axios = require('axios').default;
-let count = 0;
 
 axios.get(process.argv[2])
   .then(function (response) {
+    let count = 0;
     for (let i = 0; i < response.data.results.length; i++) {
       if (response.data.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
         count++;
